8f8fa1a8a8480386e9c986aac0c340cc51fe9df2
ibraheemganayim <Brhoma.king@gmail.com>
2024-02-04 11:42:35 +0200

    remove metric func

--- app.py
+++ app.py
@@ -19,51 +19,6 @@ s3_client = boto3.client('s3')
 with open("data/coco128.yaml", "r") as stream:
     names = yaml.safe_load(stream)['names']
 
-def metric_streamer():
-
-    sqs_client = boto3.resource('sqs', region_name='eu-west-3')
-    asg_client = boto3.client('autoscaling', region_name='eu-west-3')
-
-    AUTOSCALING_GROUP_NAME = os.environ['AGN']
-    QUEUE_NAME = os.environ['QUEUE_NAME']
-
-    queue = sqs_client.get_queue_by_name(QueueName=QUEUE_NAME)
-    msgs_in_queue = int(queue.attributes.get('ApproximateNumberOfMessages'))
-    asg_groups = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[AUTOSCALING_GROUP_NAME])[
-        'AutoScalingGroups']
-
-    if not asg_groups:
-        raise RuntimeError('Autoscaling group not found')
-    else:
-        asg_size = asg_groups[0]['DesiredCapacity']
-    if asg_size == 0:
-        asg_size = 1
-    backlog_per_instance = msgs_in_queue / asg_size
-
-    # send backlog_per_instance to cloudwatch
-    cloudwatch_client = boto3.client('cloudwatch', region_name='eu-west-3')
-    cloudwatch_client.put_metric_data(
-        Namespace='IbraheemgNamespace',
-        MetricData=[
-            {
-                'MetricName': 'BacklogPerInstance',
-                'Dimensions': [
-                    {
-                        'Name': 'AutoScalingGroupName',
-                        'Value': AUTOSCALING_GROUP_NAME
-                    },
-                ],
-                'Unit': 'Count',
-                'Value': backlog_per_instance
-            },
-        ]
-    )
-
-    return {
-        'statusCode': 200,
-        'body': f'Successfully sent backlog_per_instance metric: {backlog_per_instance}'
-    }
-
 def consume():
     
     logger.info('\nWaiting for love..\n')
